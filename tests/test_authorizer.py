e2b5fd361fac92f56925987ee377e75940dbc9b2
jmeisele <jmeisele@yahoo.com>
2023-04-03 17:23:32 +0000

    :construction_worker:

--- tests/test_authorizer.py
+++ tests/test_authorizer.py
@@ -3,7 +3,8 @@ from src.authorizer import lambda_handler
 
 def test_lambda_handler() -> None:
     event = {}
-    event["authorizationToken"] = "abc123"
+    # event["authorizationToken"] = "abc123"
+    event["headers"]["Authorization"] = "abc123"
     result = lambda_handler(event, context=None)
     expected = {
         "principalId": "abc123",
