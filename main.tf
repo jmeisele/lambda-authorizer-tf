532f7372a19b2ae6fc95c7100c9a9dce55eaf423
jmeisele <jmeisele@yahoo.com>
2023-04-03 14:39:02 +0000

    :bricks: gateway + authorizer

--- main.tf
+++ main.tf
@@ -104,3 +104,64 @@ resource "aws_lambda_function" "handler" {
   image_uri     = "${data.aws_ecr_repository.repository.repository_url}@${data.aws_ecr_image.handler_image.image_digest}"
   package_type  = "Image"
 }
+
+resource "aws_api_gateway_rest_api" "lambda_api" {
+  name = "mock_authorizer_api"
+}
+
+resource "aws_apigatewayv2_authorizer" "demo" {
+  name             = "demo_authorizer"
+  api_id           = aws_api_gateway_rest_api.demo.id
+  authorizer_type  = "REQUEST"
+  authorizer_uri   = aws_lambda_function.authorizer.invoke_arn
+  identity_sources = ["$request.header.Authorization"]
+}
+
+# resource "aws_api_gateway_resource" "proxy_pred" {
+#   rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+#   parent_id   = aws_api_gateway_rest_api.lambda_api.root_resource_id
+#   path_part   = "customer"
+#   #   request_paremeters = {
+#   #   }
+# }
+
+# resource "aws_api_gateway_method" "method_proxy" {
+#   rest_api_id   = aws_api_gateway_rest_api.lambda_api.id
+#   resource_id   = aws_api_gateway_resource.proxy_pred.id
+#   http_method   = "GET"
+#   authorization = "NONE"
+# }
+
+# resource "aws_api_gateway_integration" "api_lambda" {
+#   rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+#   resource_id = aws_api_gateway_method.method_proxy.resource_id
+#   http_method = aws_api_gateway_method.method_proxy.http_method
+
+#   integration_http_method = "POST"
+#   type                    = "AWS_PROXY"
+#   uri                     = var.lambda_func_invoke_arn
+#   timeout_milliseconds    = 29000
+# }
+
+# # IAM for API
+# resource "aws_api_gateway_rest_api_policy" "api_allow_invoke" {
+#   rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+
+#   policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Principal": "*",
+#       "Action": [
+#         "execute-api:Invoke"
+#       ],
+#       "Resource": [
+#         "arn:aws:execute-api:${var.region}:${var.account_id}:${aws_api_gateway_rest_api.lambda_api.id}/*/${aws_api_gateway_method.method_proxy.http_method}${aws_api_gateway_resource.proxy_pred.path}"
+#       ]
+#     }
+#   ]
+# }
+# EOF
+# }
